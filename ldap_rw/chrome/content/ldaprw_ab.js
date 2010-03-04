48b28471e661a381155740a87aa0a870cc65f114
ilnurathome@gmail.com <Ilnur Kiyamov>
2010-03-04 17:09:15 +0300

    Prepare to pulblish

--- ldap_rw/chrome/content/ldaprw_ab.js
+++ ldap_rw/chrome/content/ldaprw_ab.js
@@ -15,16 +15,8 @@ var ldaprw = {
 
   onMenuPrefCommand: function(e) {
     window.open("chrome://ldaprw/content/prefs.xul", "Preferences", "chrome");
-    //var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-//                                  .getService(Components.interfaces.nsIPromptService);
-//    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-  //                              this.strings.getString("helloMessage"));
   },
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    ldaprw.onMenuItemCommand(e);
-  }
 };
 
 window.addEventListener("load", ldaprw.onLoad, false);
--- ldap_rw/defaults/preferences/prefs.js
+++ ldap_rw/defaults/preferences/prefs.js
@@ -1,2 +1,1 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.ldaprw.description", "chrome://ldaprw/locale/overlay.properties");
